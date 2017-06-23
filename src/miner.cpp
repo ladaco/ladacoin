a04a7ef17e5e76313f7f1c16508014f92c80a1fc
genavarov <genavarov@gmail.com>
2017-06-23 21:08:37 +0300

    21.00 23.06
    049 miner pub key

--- src/miner.cpp
+++ src/miner.cpp
@@ -395,7 +395,16 @@ CBlockTemplate* CreateNewBlockWithKey(CReserveKey& reservekey)
     if (!reservekey.GetReservedKey(pubkey))
         return NULL;
 
+	//
+		// -minerpubkey=049...
+		// scriptPubKey = CScript() << ParseHex("0496dbe312a5db151199b7f71fe3329fdc673bcadc51dbd714ca3a70446bd628dcbb41d86252702c6b8a2d50e2fa7be835396accb7781d107d129a3dff88fcBff3") << OP_CHECKSIG;
+
+		bool MinerPubKey = GetBoolArg("-minerpubkey", MinerPubKey);
+		if (MinerPubKey = true){
+	CScript scriptPubKey = CScript() << ParseHex(MinerPubKey) << OP_CHECKSIG;
+		}else{
     CScript scriptPubKey = CScript() << ToByteVector(pubkey) << OP_CHECKSIG;
+		}
     return CreateNewBlock(scriptPubKey);
 }
 
